a5e410767678375b1cc750862874d9fd25eae704
theRadn <128877618+theRadn@users.noreply.github.com>
2025-06-02 20:20:48 +0700

    Update A02_ProgramKomnum_16.py

--- A02_ProgramKomnum_16.py
+++ A02_ProgramKomnum_16.py
@@ -1,6 +1,6 @@
 # Tugas Mata Kuliah Komputasi Numerik: Implementasi Metode Gauss-Seidel dalam Bahasa Python (Soal Nomor 16)
 
-# Kelompok A02 - Komputasi Numerik A
+# Kelompok a02 - Komputasi Numerik m
 
 # Anggota Kelompok:
 # 1. Nama: Farrel Aqilla Novianto          - NRP: 5025241015
@@ -11,12 +11,16 @@
 
 # Kode program:
 
-def gauss_seidel(iterations):
+import numpy as np
+
+def gauss_seidel(iterations, m):
     a0, a1, a2 = 0.0, 0.0, 0.0
     for i in range(1, iterations + 1):
-        a0 = (485 - 25 * a1 - 135 * a2) / 5
-        a1 = (2785 - 25 * a0 - 775 * a2) / 135
-        a2 = (16751 - 135 * a0 - 775 * a1) / 4659
+        a0 = np.round((m[0][3] - m[0][1] * a1 - m[0][2] * a2) / m[0][0], 2)
+        a1 = np.round((m[1][3] - m[1][0] * a0 - m[1][2] * a2) / m[1][1], 2)
+        a2 = np.round((m[2][3] - m[2][0] * a0 - m[2][1] * a1) / m[2][2], 2)
         print(f"Iterasi {i}: a0 = {a0:.2f}, a1 = {a1:.2f}, a2 = {a2:.2f}")
 
-gauss_seidel(2)
+matrix = np.array([[5, 25, 135, 485], [25, 135, 775, 2785], [135, 775, 4659, 16751]])
+
+gauss_seidel(2, matrix)
